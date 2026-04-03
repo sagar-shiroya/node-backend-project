99a0d14286a89ea356d71a365b2711db0078caba
Sagar Shiroya <sagar.shiroya8@gmail.com>
2026-04-04 01:36:24 +0530

    db connection configured and install dotenv, express and mongoose

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    // console.log(connectionInstance.connection);
+
+    console.log(
+      `MongoDB Connected... Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("DB Connect Error: ", error);
+    process.exit(1); // TODO: what are these numbers 1, read more...
+  }
+};
+
+export default connectDB;
